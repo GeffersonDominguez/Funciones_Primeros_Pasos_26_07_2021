9e9546fa4bc6da9e25caa123516a595e69ddbd28
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-07-28 20:41:59 -0500

    Funciones primeros pasos

---
+++ main.js
@@ -0,0 +1,60 @@
+function division(num1,num2){
+    
+    
+    return console.log(`division es=`,(num1/=num2))
+
+}
+division(
+    prompt(`inserte numero a dividir`),
+    prompt(`inserte numero2 a dividir`)
+    
+    )
+
+    function multip(num1,num2){
+    
+    
+        return console.log(`multiplicacion es=`,(num1*=num2))
+    
+    }
+    
+multip(
+    prompt(`inserte numero a multiplicar`),
+    prompt(`inserte numero2 a multiplicar`)
+        
+    )
+
+
+    function resta(num1,num2){
+    
+    
+        return console.log(`resta es=`,(num1-=num2))
+    
+    }
+    
+resta(
+    prompt(`inserte numero a restar`),
+    prompt(`inserte numero2 a restar`)
+        
+    )
+
+// function suma(num1,num2){
+//     return num1+num2;
+// }
+// function diseño(resultado){
+//     let colorConsole;
+//     if(resultado==3) 
+//         colorConsole = "color:red;";
+//     else if(resultado==4) 
+//         colorConsole = "color:blue;";
+//     else 
+//         colorConsole = "color:orange;";
+
+//     return console.log(`%cLa suma es: ${resultado}`,colorConsole);
+// }
+// diseño(suma(1,3));
+// diseño(suma(1,3));
+// diseño(suma(1,3));
+// diseño(suma(1,3));
+// diseño(suma(1,3));
+// diseño(suma(1,5));
+// diseño(suma(1,3));
